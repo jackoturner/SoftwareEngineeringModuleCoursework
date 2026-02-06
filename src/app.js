60090636b26f314e2fd33a041b6437cba2b3f7de
Luke Pring <me@lukepring.co.uk>
2026-02-06 13:14:04 +0000

    Fix formatting in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const app = express();
+const app = express(); 
  
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
