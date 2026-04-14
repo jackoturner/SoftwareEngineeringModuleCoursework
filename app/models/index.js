e095207c9e16d064623564980a939a285468073d
VictorVick22 <147448851+VictorVick22@users.noreply.github.com>
2026-04-14 18:34:38 +0100

    Login page and authentificaion

--- app/models/index.js
+++ app/models/index.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../services/db");
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
-module.exports = router;
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const sql = "SELECT * FROM users WHERE email = ? LIMIT 1";
+
+  db.query(sql, [email], (err, results) => {
+    if (err) {
+      console.error("Login query error:", err);
+      return res.status(500).send("Server error");
+    }
+
+    if (results.length === 0) {
+      return res.status(401).send("User not found");
+    }
+
+    const user = results[0];
+
+    if (password !== user.password_hash) {
+      return res.status(401).send("Incorrect password");
+    }
+
+    return res.redirect("/");
+  });
+});
+
+module.exports = router;
